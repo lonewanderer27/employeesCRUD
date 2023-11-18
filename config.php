09fbde2b4669f9614059ae2b61228a8da0583990
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-11-18 15:54:17 +0800

    add support for .env file

--- config.php
+++ config.php
@@ -1,9 +1,15 @@
 <?php
-$user = "seed";
-$pass = "seed";
-$hostname = "database";
-$db = "employees";
-$port = 3306;
+require __DIR__ . '/vendor/autoload.php';
+
+// load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$hostname = $_ENV['DB_HOST'];
+$db = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
 
 $cn = new mysqli($hostname, $user, $pass, $db, $port);
 if ($cn->connect_error) {
