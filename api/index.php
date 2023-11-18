227ebea82b8f36dfbb2bc03d2f14d854200a2e26
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-11-18 16:48:32 +0800

    add crud routes to index.php inside vercel

--- api/index.php
+++ api/index.php
@@ -1,7 +1,4 @@
 <?php
     // These are the main entrypoints for functions on vercel deployment
     require __DIR__ . '/../index.php';
-    require __DIR__ . '/../add.php';
-    require __DIR__ . '/../delete.php';
-    require __DIR__ . '/../update.php';
 ?>
